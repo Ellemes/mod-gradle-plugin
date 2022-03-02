55b913a05831728e156011217853ca48fd217d8e
NinjaPhenix <5214513+NinjaPhenix@users.noreply.github.com>
2022-03-02 14:16:26 +0000

    Update to 1.18.2 release

--- src/main/java/ninjaphenix/gradle/mod/impl/Constants.java
+++ src/main/java/ninjaphenix/gradle/mod/impl/Constants.java
@@ -5,9 +5,9 @@ import org.gradle.api.JavaVersion;
 public final class Constants {
     public static final JavaVersion JAVA_VERSION = JavaVersion.VERSION_17;
     public static final String JETBRAINS_ANNOTATIONS_VERSION = "23.0.0";
-    public static final String MINECRAFT_VERSION = "1.18.2-rc1";
+    public static final String MINECRAFT_VERSION = "1.18.2";
 
-    public static final String REQUIRED_GRADLE_VERSION = "7.4-rc-2";
+    public static final String REQUIRED_GRADLE_VERSION = "7.4";
     public static final String REQUIRED_LOOM_VERSION = "0.11.29";
     public static final String REQUIRED_FORGE_GRADLE_VERSION = "5.1.26";
     public static final String REQUIRED_MIXIN_GRADLE_VERSION = "0.7-SNAPSHOT";
