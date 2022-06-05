0560296345d5f4a06cb55321db8b93424a60ba2a
Ellemes <3llemes@gmail.com>
2022-06-05 16:22:41 +0100

    Disable pre-release checks due to them not working properly still.

--- src/main/java/ellemes/gradle/mod/impl/GradlePlugin.java
+++ src/main/java/ellemes/gradle/mod/impl/GradlePlugin.java
@@ -75,53 +75,53 @@ public final class GradlePlugin implements Plugin<Project> {
         target.getExtensions().configure(ArchitectPluginExtension.class, extension -> extension.setMinecraft(minecraftVersion));
         Task buildTask = target.task("buildMod");
         Task releaseTask = target.task("releaseMod", task -> {
-            if (!System.getProperty("MOD_IGNORE_CHANGES", "false").equalsIgnoreCase("false")) {
-                return;
-            }
-            Exception error = null;
-            boolean hasChanges = false;
-            try {
-                Process process = new ProcessBuilder()
-                        .directory(target.getProjectDir())
-                        .command("git", "status", "--porcelain")
-                        .start();
-                process.waitFor();
-                try (BufferedReader reader = process.inputReader(StandardCharsets.UTF_8)) {
-                    hasChanges = reader.readLine() != null;
-                }
-            } catch (IOException | InterruptedException e) {
-                error = e;
-            }
-            if (hasChanges) {
-                throw new IllegalStateException("Cannot release with uncommitted changes.");
-            } else if (error != null) {
-                throw new IllegalStateException("Error occurred whilst checking for uncommitted changes.", error);
-            }
-            boolean sameAsRemote = false;
-            try {
-                Process process = new ProcessBuilder()
-                        .directory(target.getProjectDir())
-                        .command("git", "status", "-b", "--porcelain=2")
-                        .start();
-                process.waitFor();
-                try (BufferedReader reader = process.inputReader(StandardCharsets.UTF_8)) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        String[] parts = line.split(" ");
-                        if (parts.length >= 2 && "branch.ab".equals(parts[1])) {
-                            sameAsRemote = parts[2].equals("+0") && parts[3].equals("-0");
-                            break;
-                        }
-                    }
-                }
-            } catch (IOException | InterruptedException e) {
-                error = e;
-            }
-            if (!sameAsRemote) {
-                throw new IllegalStateException("Cannot release with un-pushed changes.");
-            } else if (error != null) {
-                throw new IllegalStateException("Error occurred whilst checking for un-pushed changes.", error);
-            }
+            //if (!System.getProperty("MOD_IGNORE_CHANGES", "false").equalsIgnoreCase("false")) {
+            //    return;
+            //}
+            //Exception error = null;
+            //boolean hasChanges = false;
+            //try {
+            //    Process process = new ProcessBuilder()
+            //            .directory(target.getProjectDir())
+            //            .command("git", "status", "--porcelain")
+            //            .start();
+            //    process.waitFor();
+            //    try (BufferedReader reader = process.inputReader(StandardCharsets.UTF_8)) {
+            //        hasChanges = reader.readLine() != null;
+            //    }
+            //} catch (IOException | InterruptedException e) {
+            //    error = e;
+            //}
+            //if (hasChanges) {
+            //    throw new IllegalStateException("Cannot release with uncommitted changes.");
+            //} else if (error != null) {
+            //    throw new IllegalStateException("Error occurred whilst checking for uncommitted changes.", error);
+            //}
+            //boolean sameAsRemote = false;
+            //try {
+            //    Process process = new ProcessBuilder()
+            //            .directory(target.getProjectDir())
+            //            .command("git", "status", "-b", "--porcelain=2")
+            //            .start();
+            //    process.waitFor();
+            //    try (BufferedReader reader = process.inputReader(StandardCharsets.UTF_8)) {
+            //        String line;
+            //        while ((line = reader.readLine()) != null) {
+            //            String[] parts = line.split(" ");
+            //            if (parts.length >= 2 && "branch.ab".equals(parts[1])) {
+            //                sameAsRemote = parts[2].equals("+0") && parts[3].equals("-0");
+            //                break;
+            //            }
+            //        }
+            //    }
+            //} catch (IOException | InterruptedException e) {
+            //    error = e;
+            //}
+            //if (!sameAsRemote) {
+            //    throw new IllegalStateException("Cannot release with un-pushed changes.");
+            //} else if (error != null) {
+            //    throw new IllegalStateException("Error occurred whilst checking for un-pushed changes.", error);
+            //}
         });
 
         target.subprojects(project -> {
